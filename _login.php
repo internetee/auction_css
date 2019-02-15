6272faa1c224fe608a9c5f25e2479dc9bf6e48b6
Ceiro Kilp <ceiro.kilp@we.ee>
2019-02-15 09:29:52 +0200

    Change login page components

--- _login.php
+++ _login.php
@@ -3,43 +3,33 @@
   <h1>Logi sisse</h1>
   <p>Sisenemine võimalik Eesti riigi ja e-residentide ID-kaardi ja Mobiil-IDga, Smart IDga ning EU EIDAS süsteemiga liitunud riikide elektroonsete isikutuvastusvahenditega.</p>
 </div>
-<div class="login-blocks">
-  <div class="u-container">
-    <div class="login-blocks__list">
-      <div class="login-blocks__item">
-        <div class="login-block ui segment">
-          <form class="ui form big">
-            <h3>Sisene elektroonilise ID-ga</h3>
-            <p>Sind suunatakse TARA autentimisteenusesse.</p>
-            <div class="form-actions form-actions--mt-sm">
-              <button class="ui primary button big">Sisene</button>
-            </div>
-          </form>
+<div class="u-container">
+  <div class="content center">
+    <div class="ui card raised">
+      <div class="content">
+        <div class="header"><h3>Sisene elektroonilise ID-ga</h3></div>
+        <div class="description">
+          <p>Sind suunatakase TARA autentimisteenusesse.</p>
+          <button class="ui primary button big">Sisene</button>
         </div>
-      </div>      
-      <div class="login-blocks__item">
-        <div class="login-block ui segment">
+      </div>
+    </div>
+    <div class="ui card raised">
+      <div class="content">
+        <div class="header"><h3>Sisene kontoga</h3></div>
+        <div class="description">
           <form class="ui form big">
-            <h3>Sisene kontoga</h3>
             <div class="field">
               <label>Kasutajanimi</label>
               <input type="text">
             </div>
             <div class="field">
               <label>Parool</label>
-              <input type="password">
-            </div>
-            <div class="form-actions">
-              <button class="ui primary button big">Sisene</button>
+              <input type="text">
             </div>
           </form>
+          <button class="ui primary button big">Sisene</button>
         </div>
-      </div>
-    </div>
-    <div class="login-blocks__register">
-      <h2>Ei oma sisselogimisvõimalust?</h2>
-      <div class="login-blocks__register__buttons">
-        <button class="ui green button big">Loo konto</button>
       </div>
     </div>
   </div>
